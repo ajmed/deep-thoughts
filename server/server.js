095017faee0bb4fce14e0e619f8f5ec6b69e425c
Alexander Medeiros <amedeiros@soundexchange.com>
2021-08-27 18:10:21 -0400

    #3

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 (async () => {
+    const path = require('path');
     const { authMiddleware } = require('./utils/auth');
     const express = require('express')
     // import ApolloServer
@@ -24,6 +25,14 @@
 
     app.use(express.urlencoded({extended: false}))
     app.use(express.json())
+    // Serve up static assets
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/build')));
+    }
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
 
     db.once('open', () => {
         app.listen(PORT, () => {
