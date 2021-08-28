6ae7d391753a0141b152209b3b6df579f7681d01
Alexander Medeiros <amedeiros@soundexchange.com>
2021-08-27 20:19:49 -0400

    #6

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import { Redirect, useParams } from 'react-router-dom';
-import {useQuery} from '@apollo/client'
+import {Redirect, useParams} from 'react-router-dom'
+import {useMutation, useQuery} from '@apollo/react-hooks'
 import {QUERY_ME, QUERY_USER} from '../utils/queries'
 import ThoughtList from '../components/ThoughtList'
 import FriendList from '../components/FriendList'
-import Auth from '../utils/auth';
-
+import Auth from '../utils/auth'
+import {ADD_FRIEND} from '../utils/mutations'
+import ThoughtForm from '../components/ThoughtForm';
 
 const Profile = () => {
+    const [addFriend] = useMutation(ADD_FRIEND)
     const {username: userParam} = useParams()
     const {loading, data} = useQuery(userParam ? QUERY_USER : QUERY_ME, {
         variables: {username: userParam}
     })
 
-    const user = data?.me || data?.user || {};
+    const user = data?.me || data?.user || {}
     // redirect to personal profile page if username is the logged-in user's
     if (Auth.loggedIn() && userParam && (Auth.getProfile().data.username.toLowerCase() === userParam.toLowerCase())) {
-        return <Redirect to="/profile" />;
+        return <Redirect to="/profile"/>
     }
 
     if (loading) {
@@ -28,7 +30,17 @@ const Profile = () => {
             <h4>
                 You need to be logged in to see this page. Use the navigation links above to sign up or log in!
             </h4>
-        );
+        )
+    }
+
+    const handleClick = async () => {
+        try {
+            let data = await addFriend({
+                variables: {id: user._id}
+            })
+        } catch (e) {
+            console.error(e)
+        }
     }
 
     return (
@@ -37,6 +49,12 @@ const Profile = () => {
                 <h2 className="bg-dark text-secondary p-3 display-inline-block">
                     Viewing {userParam ? `${user.username}'s` : 'your'} profile.
                 </h2>
+
+                {userParam && (
+                    <button className="btn ml-auto" onClick={handleClick}>
+                        Add Friend
+                    </button>
+                )}
             </div>
 
             <div className="flex-row justify-space-between mb-3">
@@ -52,6 +70,7 @@ const Profile = () => {
                     />
                 </div>
             </div>
+            <div className="mb-3">{!userParam && <ThoughtForm />}</div>
         </div>
     )
 }
